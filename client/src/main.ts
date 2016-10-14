418d353483d7d6dc4b65d445f755ed7af0517a9a
Bruno Kirschner <bruno.kirschner@online.de>
2016-10-14 09:55:21 +0200

    [General] Renames all .js files into .ts files.

---
+++ client/src/main.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+import "babel-polyfill"
+
+import Blinder from "./blinding/blinder"
+import blinding_util from "./blinding/blinding_util"
+import client from"./client"
+import pgp from "./pgp/pgp"
+
+if (document && document.getElementById("activate_pseudonym_button")) {
+  
+  const button : HTMLElement | null = document.getElementById("activate_pseudonym_button");
+
+  if (button instanceof HTMLElement) {
+    button.onclick = async () => {
+      const { blinder, packet } = await blinding_util.prepareBlinding();
+      await blinder.forgeSignature(packet);
+
+      const key_ascii = await pgp.exportKeyToAsciiWithSignature(packet.target_key, packet);
+      console.log(key_ascii);
+    }
+  }
+}
